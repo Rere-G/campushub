2af2e8e20b80073aa460edfc4e9e524dd2479301
Rere-G <thawdarn4@gmail.com>
2026-09-20 15:23:44 +0630

    Enable root route redirect; Firestore rules deployed

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Marketplace from "./pages/Marketplace";
 import Services from "./pages/Services";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
-import Profile from "./pages/Profile"
+import Profile from "./pages/Profile";
 
 export default function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/Home" element={<Home />} />
       <Route path="/marketplace" element={<Marketplace />} />
       <Route path="/services" element={<Services />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/Profile" element={<Profile />} />   
+      <Route path="/Profile" element={<Profile />} />
+      <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
 }
